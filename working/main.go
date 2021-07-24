2ab24ea181cadda3aa72211fc8e64d48df97a5c2
johan-STYL <johan@styldesign.se>
2021-07-24 20:17:47 +0200

    wip

--- working/main.go
+++ working/main.go
@@ -4,11 +4,15 @@ import (
 	"log"
 	"net/http"
 	"os"
+	// "github.com/johan-st/try-go/working/handlers"
 )
 
 func main() {
 	l := log.New(os.Stdout, "product-api", log.LstdFlags)
 	helloHandler := handlers.newHello(l)
+
+	sm := http.NewServeMux()
+	sm.Handle("/", helloHandler)
 	// http.HandleFunc("/", func(rw http.ResponseWriter, r *http.Request) {
 	// 	handlers.Hello(log)
 	// })
@@ -17,5 +21,5 @@ func main() {
 	// 	log.Printf("Goodbye World")
 	// })
 
-	http.ListenAndServe(":9090", nil)
+	http.ListenAndServe(":9090", sm)
 }
